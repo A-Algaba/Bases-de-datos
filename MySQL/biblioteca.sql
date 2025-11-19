7431c2fcce1acd96dcabb6ada8c66afdf2d90390
A-Algaba <aac0077@alu.medac.es>
2025-11-19 14:54:08 +0100

    update biblioteca.sql

--- MySQL/biblioteca.sql
+++ MySQL/biblioteca.sql
@@ -3,7 +3,7 @@ CREATE DATABASE biblioteca;
 USE biblioteca;
 
 create table autor (
-	codigo_autor int (5) primary key,
+	codigo_autor int primary key, -- los valores de int o varchar tienen que coincidir con la clave foranea para hacer relacion entre tablas
     nombre varchar (50) not null
 );
 
@@ -11,23 +11,23 @@ create table libro (
 	ISBN varchar (10) primary key,
     titulo varchar (100) not null,
     anio date not null,
-    cod_autor int (5),
+    cod_autor int,
     foreign key(cod_autor) references autor(codigo_autor)
 );
 
+create table socio (
+	codigo_socio int primary key,
+    nombre varchar (15),
+    apellidos varchar (20),
+    telefono int
+);
+
 create table prestamo (
-	codigo int (7) primary key auto_increment,
+	codigo int primary key auto_increment,
     fecha_prestamo date not null,
     fecha_devolucion date,
     libro_prestamo varchar (10),
     foreign key (libro_prestamo) references libro(ISBN)
-);
-
-create table socio (
-	codigo_socio int (5) primary key,
-    nombre varchar (15),
-    apellidos varchar (20),
-    telefono int (9)
 );
 
 -- añadimos nuevas columnas con los siguientes comandos
@@ -38,7 +38,7 @@ alter table libro modify titulo varchar(150);
 
 alter table socio modify telefono varchar(9) not null;
 
-alter table prestamo add cod_socio int (5);
+alter table prestamo add cod_socio int;
 
 alter table prestamo add constraint fk_socio foreign key (cod_socio) references socio (codigo_socio) on update cascade;
 
