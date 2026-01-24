310755d2d5aa716c6737fdd6bd25dfee1de67cf3
A-Algaba <aac0077@alu.medac.es>
2026-01-24 21:10:03 +0100

    update consultas, esta vez  hay mas subconsultas que hay que practicar

--- MySQL/Consultas_SQL/consultas_universidad.sql
+++ MySQL/Consultas_SQL/consultas_universidad.sql
@@ -70,3 +70,48 @@ select p.id_profesor, per.nombre as nombre_profesor ,a.nombre from profesor p  l
 select a.nombre as nombre_asignatura, per.nombre as nombre_profesor 
 from persona per join profesor pro on (per.id = pro.id_profesor) 
 right join asignatura a on (a.id = pro.id_profesor);
+
+--  1. Obtener el nombre y apellidos de todos los alumnos, junto con el nombre del grado que cursan.
+
+select per.nombre, per.apellido1, per.apellido2, g.nombre 
+from persona per join alumno_se_matricula_asignatura am on (per.id = am.id_alumno) 
+join asignatura a on (a.id = am.id_asignatura) 
+join grado g on (g.id = a.id_grado);
+
+-- 2. Mostrar el nombre de cada asignatura y el nombre del grado al que pertenece.
+
+select a.nombre as nombre_asignatura, g.nombre as nombre_grado from asignatura a join grado g on (a.id_grado = g.id);
+
+-- 3. Listar todas las asignaturas junto con el nombre y apellidos del profesor que las imparte.
+
+select a.nombre as nombre_asignatura, per.nombre as nombre_profesor, per.apellido1, per.apellido2 as nombre_asignatura 
+from asignatura a join profesor pro on (pro.id_profesor = a.id_profesor) 
+join persona per on (pro.id_profesor = per.id);
+
+-- 4. Obtener el nombre de los profesores y el nombre del departamento al que pertenecen.
+
+select per.nombre as nombre_profesor, d.nombre as nombre_departamennto 
+from persona per join profesor pro on (per.id = pro.id_profesor)
+join departamento d on (pro.id_departamento = d.id);
+
+-- 5.Obtener el nombre de las asignaturas en las que esté matriculado el alumno llamado “Sonia”.
+
+select * from persona per right join alumno_se_matricula_asignatura am on (am.id_alumno = per.id) right join asignatura a on (a.id = am.id_asignatura) where per.nombre = "sonia";
+
+-- 6. Listar los departamentos que no tienen profesores asociados.
+
+select d.nombre from departamento d where d.id not in (select distinct p.id_departamento from profesor p where p.id_departamento is not null);
+
+-- 7.Mostrar el nombre de las asignaturas que pertenecen al Grado en Ingeniería Informática.alter
+
+select * from asignatura where id_grado = (select id from grado where id = 4);
+
+-- 8. Obtener los nombres de los profesores que pertenecen al departamento de Informática.
+
+select * from profesor where id_departamento = (select id from departamento where nombre = "informatica");
+
+-- 9. Mostrar los alumnos matriculados en el curso escolar 2018–2019.
+
+select * from alumno_se_matricula_asignatura where id_curso_escolar = (select id from curso_escolar where anyo_inicio = 2018 and anyo_fin = 2019);
+
+	
